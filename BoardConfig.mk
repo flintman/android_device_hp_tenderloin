c289c9e7a812a46c2b72125f4b6789545fd79392
William Bellavance <flintman@flintmancomputers.com>
2018-02-24 08:11:27 -0500

    combine common into tenderloin and setup only for recovery

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,7 +1,144 @@
-# inherit from common tenderloin
--include device/hp/tenderloin-common/BoardConfigCommon.mk
+# inherit from the proprietary version
+-include vendor/hp/tenderloin/BoardConfigVendor.mk
+-include hardware/atheros/ath6kl/firmware/device-ath6kl.mk
+
 
 # Define Prebuilt kernel locations
-TARGET_PREBUILT_KERNEL := device/hp/tenderloin-common/prebuilt/boot/kernel
+TARGET_PREBUILT_KERNEL := device/hp/tenderloin/prebuilt/boot/kernel
 
 TARGET_KERNEL_CONFIG := tenderloin_recovery_defconfig
+
+TARGET_SPECIFIC_HEADER_PATH := device/hp/tenderloin/include 
+
+# Platform
+TARGET_BOARD_PLATFORM := msm8660
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
+
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_SMP := true
+TARGET_CPU_VARIANT := cortex-a8
+TARGET_HAVE_TSLIB := false
+
+# Art
+LIBART_IMG_BASE := 0x57000000
+WITH_DEXPREOPT := true
+
+# Binder API version
+TARGET_USES_64_BIT_BINDER := true
+
+# Bootloader
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+TARGET_BOOTLOADER_BOARD_NAME := tenderloin
+
+# We have so much memory 3:1 split is detrimental to us.
+TARGET_USES_2G_VM_SPLIT := true
+
+
+# Compatibilty
+TARGET_NEEDS_NON_PIE_SUPPORT := true
+TARGET_DISABLE_ARM_PIE := true
+TARGET_NEEDS_PRELINK_SUPPORT := true
+TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS := true
+TARGET_HAS_LEGACY_CAMERA_HAL1 := true
+TARGET_HAS_NO_CAMERA_FLASH := true
+
+# Device manifest
+DEVICE_MANIFEST_FILE := device/hp/tenderloin/manifest.xml
+DEVICE_MATRIX_FILE := device/hp/tenderloin/compatibility_matrix.xml
+
+# Display
+USE_OPENGL_RENDERER := true
+BOARD_USES_LEGACY_MMAP := true
+TARGET_USES_ION := true
+BOARD_EGL_WORKAROUND_BUG_10194508 := true
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
+TARGET_DISPLAY_INSECURE_MM_HEAP := true
+TARGET_DISPLAY_USE_RETIRE_FENCE := true
+TARGET_NO_INITLOGO := true
+TARGET_USES_HWC2 := true
+SF_START_GRAPHICS_ALLOCATOR_SERVICE := true
+VSYNC_EVENT_PHASE_OFFSET_NS := 2000000
+SF_VSYNC_EVENT_PHASE_OFFSET_NS := 6000000
+
+# Dosfsck from dosfstools
+BOARD_USES_CUSTOM_FSCK_MSDOS := true
+
+# GPS
+BOARD_USES_QCOM_GPS := true
+BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 50000
+
+# Kernel
+BOARD_CUSTOM_BOOTIMG_MK := device/hp/tenderloin/uboot-bootimg.mk
+BOARD_USES_UBOOT := true
+BOARD_USES_UBOOT_MULTIIMAGE := true
+BOARD_KERNEL_BASE := 0x40200000
+BOARD_KERNEL_CMDLINE := console=none
+BOARD_KERNEL_IMAGE_NAME := uImage
+BOARD_PAGE_SIZE := 2048
+TARGET_KERNEL_SOURCE := kernel/htc/msm8960
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
+KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
+TARGET_KERNEL_NO_MODULES := true
+
+# Logging
+BOARD_NEEDS_CUTILS_LOG := true
+BOARD_USES_ALT_KMSG_LOCATION := "/proc/last_klog"
+
+# Malloc
+MALLOC_SVELTE := true
+
+# Media
+TARGET_NO_ADAPTIVE_PLAYBACK := true
+
+# Partitions
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16776192
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1375731712  #Update to 1312
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 20044333056
+BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_REQUIRES_FORCE_VPARTITION := true
+
+# Qualcomm support
+BOARD_USES_QCOM_HARDWARE := true
+
+# Recovery
+BOARD_HAS_SDCARD_INTERNAL := false
+BOARD_USES_MMCUTILS := true
+BOARD_HAS_NO_MISC_PARTITION := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_HAS_NO_REAL_SDCARD := true
+RECOVERY_FSTAB_VERSION := 2
+RECOVERY_SDCARD_ON_DATA := true
+TARGET_RECOVERY_FSTAB := device/hp/tenderloin/rootdir/etc/fstab.tenderloin
+TARGET_RECOVERY_DEVICE_DIRS := device/hp/tenderloin
+TARGET_RECOVERY_PRE_COMMAND := "/system/bin/rebootcmd"
+
+# Releasetools
+TARGET_PROVIDES_RELEASETOOLS := true
+TARGET_RELEASETOOL_IMG_FROM_TARGET_SCRIPT := device/hp/tenderloin/releasetools/tenderloin_img_from_target_files
+TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := device/hp/tenderloin/releasetools/tenderloin_ota_from_target_files
+
+# Twrp
+DEVICE_RESOLUTION = 1024x768
+TW_INTERNAL_STORAGE_PATH := "/data/media/0"
+TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
+TW_EXTERNAL_STORAGE_PATH := "/external_sd"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+TW_NO_SCREEN_BLANK := true
+TW_NO_REBOOT_BOOTLOADER := true
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_WHITELIST_INPUT := "HPTouchpad"
+TW_NO_CPU_TEMP := true
+TW_EXCLUDE_SUPERSU := true
+TW_EXCLUDE_ENCRYPTED_BACKUPS :=true
+
+# Vold
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/msm_hsusb/gadget/lun0/file"
+
--- full_tenderloin.mk
+++ full_tenderloin.mk
@@ -1,31 +1,8 @@
-#
-# Copyright (C) 2011 The Evervolv Project
-# Copyright (C) 2011 The CyanogenMod Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
-
 # Include tenderloin's BoardConfig.mk
 include device/hp/tenderloin/BoardConfig.mk
 
 # Proprietary stuff.
 $(call inherit-product-if-exists, vendor/hp/tenderloin/tenderloin-vendor.mk)
-
-# common tenderloin configs
-$(call inherit-product, device/hp/tenderloin-common/tenderloin-common.mk)
-
-DEVICE_PACKAGE_OVERLAYS += device/hp/tenderloin/overlay
 
 
 PRODUCT_COPY_FILES += \
@@ -33,3 +10,180 @@ PRODUCT_COPY_FILES += \
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# AndroidGO
+$(call inherit-product, build/target/product/go_defaults_512.mk)
+$(call inherit-product, build/target/product/generic_no_telephony.mk)
+
+DEVICE_PACKAGE_OVERLAYS += device/hp/tenderloin/overlay
+
+# Aapt
+PRODUCT_CHARACTERISTICS := tablet
+PRODUCT_AAPT_CONFIG := xlarge mdpi
+#PRODUCT_AAPT_PREF_CONFIG := mdpi
+
+#Art
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=512k \
+    dalvik.vm.heapmaxfree=8m
+
+# Bootloader
+PRODUCT_COPY_FILES += \
+    device/hp/tenderloin/moboot_control.sh:system/bin/moboot_control.sh
+
+# Ramdisk
+PRODUCT_PACKAGES += \
+    fstab.tenderloin \
+    init.tenderloin.rc \
+    init.tenderloin.power.rc \
+    init.tenderloin.usb.rc \
+    ueventd.tenderloin.rc
+
+
+# Display
+PRODUCT_PACKAGES += \
+    copybit.msm8660 \
+    gralloc.msm8660 \
+    hwcomposer.msm8660 \
+    libgenlock \
+    memtrack.msm8660
+
+PRODUCT_PROPERTY_OVERRIDES += debug.hwui.use_buffer_age=false
+
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.mapper@2.0-impl
+
+# Drm
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl
+
+# Filesystem management tools
+PRODUCT_PACKAGES += \
+    fsck.f2fs \
+    resize2fs_static
+
+#GNSS HAL
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@1.0-impl
+
+# Init.d
+PRODUCT_COPY_FILES += \
+    device/hp/tenderloin/prebuilt/etc/init.d/10check_media_minor:system/etc/init.d/10check_media_minor
+
+# IO Scheduler
+PRODUCT_PROPERTY_OVERRIDES += \
+    sys.io.scheduler=bfq
+
+# Keylayouts
+PRODUCT_COPY_FILES += \
+    device/hp/tenderloin/prebuilt/usr/idc/HPTouchpad.idc:system/usr/idc/HPTouchpad.idc \
+    device/hp/tenderloin/prebuilt/usr/keylayout/Generic.kl:system/usr/keylayout/Generic.kl \
+    device/hp/tenderloin/prebuilt/usr/keylayout/pmic8058_pwrkey.kl:system/usr/keylayout/pmic8058_pwrkey.kl
+
+# Keymaster HAL
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@3.0-impl
+
+# Lights
+PRODUCT_PACKAGES += \
+    lights.tenderloin \
+    android.hardware.light@2.0-impl
+
+# Low-RAM optimizations
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.force_highendgfx=true \
+    dalvik.vm.jit.codecachesize=0 \
+    ro.config.max_starting_bg=4 \
+    ro.sys.fw.bg_apps_limit=8 \
+    ro.sys.fw.use_trim_settings=true \
+    ro.sys.fw.empty_app_percent=50 \
+    ro.sys.fw.trim_empty_percent=100 \
+    ro.sys.fw.trim_cache_percent=100 \
+    ro.sys.fw.trim_enable_memory=874512384 \
+    ro.sys.fw.bservice_enable=true \
+    ro.sys.fw.bservice_limit=5 \
+    ro.sys.fw.bservice_age=5000
+
+# Memtrack HAL
+PRODUCT_PACKAGES += \
+    android.hardware.memtrack@1.0-impl
+
+# OMX
+PRODUCT_PACKAGES += \
+    libOmxCore \
+    libOmxVdec \
+    libOmxVenc \
+    libOmxAacEnc \
+    libOmxAmrEnc \
+    libOmxEvrcEnc \
+    libOmxQcelp13Enc \
+    libstagefrighthw
+
+# OMX properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.media.treble_omx=false
+
+# Prebuilts
+PRODUCT_COPY_FILES += \
+    device/hp/tenderloin/prebuilt/tptoolbox.cfg:tptoolbox.cfg \
+    device/hp/tenderloin/prebuilt/boot/moboot.splash.Evervolv.tga:moboot.splash.Evervolv.tga \
+    device/hp/tenderloin/prebuilt/boot/moboot.default:moboot.default \
+    device/hp/tenderloin/prebuilt/lvm/lvm.conf:root/lvm/lvm.conf \
+    device/hp/tenderloin/prebuilt/gapps-config.txt:install/.gapps-config-tenderloin.txt \
+
+# Power
+PRODUCT_PACKAGES += \
+    android.hardware.power@1.0-impl \
+    power.tenderloin
+
+# Recovery
+PRODUCT_COPY_FILES += \
+    device/hp/tenderloin/releasetools/install-recovery.sh:$(PRODUCT_OUT)/ota_temp/SYSTEM/bin/install-recovery.sh
+
+# RenderScript HAL
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
+
+# Sensors
+PRODUCT_PACKAGES += \
+    sensors.tenderloin
+
+# Sensor HAL
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@1.0-impl
+
+# System properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.lcd_density=160 \
+    dalvik.vm.dex2oat-flags=--no-watch-dog \
+    dalvik.vm.dex2oat-swap=false \
+    dalvik.vm.image-dex2oat-filter=speed \
+    ro.com.google.networklocation=1 \
+    media.stagefright.legacyencoder=true \
+    media.stagefright.less-secure=true \
+    camera2.portability.force_api=1 \
+    qcom.hw.aac.encoder=true \
+    debug.composition.type=dyn \
+    persist.hwc.mdpcomp.enable=false \
+    ro.opengles.version=131072 \
+    debug.sf.disable_backpressure=1
+
+# Tools
+PRODUCT_PACKAGES += \
+    dosfsck \
+    librs_jni \
+    libmllite \
+    libmlplatform \
+    ts_srv \
+    ts_srv_set \
+    mkbootimg
+
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl
+

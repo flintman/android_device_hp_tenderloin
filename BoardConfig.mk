5359d564794376bdf71757ae1d0103d658e344a1
William Bellavance <flintman@flintmancomputers.com>
2018-08-17 06:03:05 -0400

    remove old prebuild kernel tag

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,9 +1,6 @@
 # inherit from common tenderloin
 -include device/hp/tenderloin-common/BoardConfigCommon.mk
 
-# Define Prebuilt kernel locations
-TARGET_PREBUILT_KERNEL := device/hp/tenderloin-common/prebuilt/boot/kernel
-
 ifdef RECOVERY_BUILD
 TARGET_KERNEL_CONFIG := tenderloin_recovery_defconfig
 else
